9951a35fc9933c940089a4c6ec9a527038a70600
ftnext <takuyafjp+develop@gmail.com>
2023-08-24 01:42:34 +0900

    [docs] Add CLI help

--- hayasaka/__main__.py
+++ hayasaka/__main__.py
@@ -1,13 +1,40 @@
 import argparse
+from textwrap import dedent
 
 from hayasaka.cli import as_uri
 from hayasaka.core import take_screenshot
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser_help = """
+    Save the rendered HTML as an image.
+
+    Examples:
+
+    - from URL
+
+      %(prog)s https://example.com/your/slide.html awesome.png
+
+    - from local HTML file
+
+      %(prog)s your/slide.html awesome.png
+
+      tips: Also supports
+
+      %(prog)s file:///home/.../your/slide.html awesome.png
+    """
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=dedent(parser_help),
+    )
     parser.add_argument(
-        "uri_or_path", help="Example: https://... file://...", type=as_uri
+        "uri_or_path",
+        help=(
+            "Specify an HTML file on Web (https://...) "
+            "or your computer (file path or file://...)"
+        ),
+        type=as_uri,
     )
     parser.add_argument("screenshot_path")
     args = parser.parse_args()
