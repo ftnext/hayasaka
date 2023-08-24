86bfce10bec8308dd334563b2a325472aad1d3b3
ftnext <takuyafjp+develop@gmail.com>
2023-08-24 20:59:08 +0900

    [refactor] Extract options builder function

--- hayasaka/core.py
+++ hayasaka/core.py
@@ -3,11 +3,16 @@ from selenium.webdriver import FirefoxOptions
 
 
 def take_screenshot(url: str, save_path: str) -> None:
-    options = FirefoxOptions()
-    options.add_argument("--width=800")
-    options.add_argument("--height=485")  # ブラウザウィンドウ上部が85px分ある
-
+    options = build_options()
     start_firefox(options=options, headless=True)
     go_to(url)
     get_driver().save_screenshot(save_path)
     kill_browser()
+
+
+def build_options() -> FirefoxOptions:
+    options = FirefoxOptions()
+    options.add_argument("--width=800")
+    options.add_argument("--height=485")  # ブラウザウィンドウ上部が85px分ある
+
+    return options
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,6 @@
-from unittest.mock import call, patch
+from unittest.mock import patch
+
+from selenium.webdriver import FirefoxOptions
 
 from hayasaka import core
 
@@ -7,21 +9,25 @@ from hayasaka import core
 @patch("hayasaka.core.get_driver")
 @patch("hayasaka.core.go_to")
 @patch("hayasaka.core.start_firefox")
-@patch("hayasaka.core.FirefoxOptions")
+@patch("hayasaka.core.build_options")
 def test_take_screenshot(
-    FirefoxOptions, start_firefox, go_to, get_driver, kill_browser
+    build_options, start_firefox, go_to, get_driver, kill_browser
 ):
-    options = FirefoxOptions.return_value
+    options = build_options.return_value
     driver = get_driver.return_value
 
     core.take_screenshot("https://example.com/awesome", "save/to/path.png")
 
-    FirefoxOptions.assert_called_once_with()
-    options.add_argument.assert_has_calls(
-        [call("--width=800"), call("--height=485")]
-    )
+    build_options.assert_called_once_with()
     start_firefox.assert_called_once_with(options=options, headless=True)
     go_to.assert_called_once_with("https://example.com/awesome")
     get_driver.assert_called_once_with()
     driver.save_screenshot.assert_called_once_with("save/to/path.png")
     kill_browser.assert_called_once_with()
+
+
+def test_build_options():
+    actual = core.build_options()
+
+    assert isinstance(actual, FirefoxOptions)
+    assert set(actual.arguments) == {"--width=800", "--height=485"}
