63692c7cd1a48e6fd5a845d915beb26bc108dc23
ftnext <takuyafjp+develop@gmail.com>
2023-08-26 02:25:55 +0900

    [feat] Say like Hayasaka Sora san

--- hayasaka/__main__.py
+++ hayasaka/__main__.py
@@ -5,6 +5,7 @@ from hayasaka.core import take_screenshot
 def main():
     args = parse_args()
     take_screenshot(args.uri_or_path, args.screenshot_path)
+    print(f"Got the best shot: {args.screenshot_path}")
 
 
 if __name__ == "__main__":
