385d3f25e89809ec61f2c14ca3c53e1d32229fd9
ftnext <takuyafjp+develop@gmail.com>
2023-09-01 22:56:03 +0900

    [feat] Draft chrome.take_screenshot

--- hayasaka/chrome.py
+++ hayasaka/chrome.py
@@ -1,4 +1,7 @@
+from helium_cylinder.decorators import using_chrome
 from selenium.webdriver import ChromeOptions
+
+from hayasaka.core import take_screenshot as _take_screenshot
 
 
 def build_options() -> ChromeOptions:
@@ -6,3 +9,8 @@ def build_options() -> ChromeOptions:
     options.add_argument("--window-size=400,200")  # 保存される画像のサイズは2倍になる
 
     return options
+
+
+@using_chrome(options=build_options(), headless=True)
+def take_screenshot(url: str, save_path: str):
+    _take_screenshot(url, save_path)
--- tests/test_chrome.py
+++ tests/test_chrome.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from unittest.mock import ANY, patch
+
 from selenium.webdriver import ChromeOptions
 
 from hayasaka import chrome
@@ -14,3 +16,17 @@ def test_build_options():
     actual = chrome.build_options()
 
     assert_options_arguments(actual, {"--window-size=400,200"})
+
+
+@patch("hayasaka.chrome._take_screenshot")
+@patch("helium_cylinder.decorators.start_chrome")
+def test_take_screenshot(start_chrome, _take_screenshot):
+    chrome.take_screenshot("https://example.com/awesome", "save/to/path.png")
+
+    start_chrome.assert_called_once_with(options=ANY, headless=True)
+    assert_options_arguments(
+        start_chrome.call_args.kwargs["options"], {"--window-size=400,200"}
+    )
+    _take_screenshot.assert_called_once_with(
+        "https://example.com/awesome", "save/to/path.png"
+    )
