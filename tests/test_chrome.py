14fc7ab9022f5cdbec2654a030057be8a0927602
ftnext <takuyafjp+develop@gmail.com>
2023-09-01 22:51:47 +0900

    [refactor] Extract ChromeOption assertion

--- tests/test_chrome.py
+++ tests/test_chrome.py
@@ -1,10 +1,16 @@
+from __future__ import annotations
+
 from selenium.webdriver import ChromeOptions
 
 from hayasaka import chrome
 
 
+def assert_options_arguments(option, expected_arguments: set[str]) -> None:
+    assert isinstance(option, ChromeOptions)
+    assert set(option.arguments) == expected_arguments
+
+
 def test_build_options():
     actual = chrome.build_options()
 
-    assert isinstance(actual, ChromeOptions)
-    assert set(actual.arguments) == {"--window-size=400,200"}
+    assert_options_arguments(actual, {"--window-size=400,200"})
